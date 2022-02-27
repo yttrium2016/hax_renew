49795fec6c8b00c1399e2b9ac5fd0d36b36b7be0
nazo <44923159+nazo-x1@users.noreply.github.com>
2022-02-27 23:58:27 +0800

    remove old selenium code

--- simpleCaptchaSolver.py
+++ simpleCaptchaSolver.py
@@ -1,15 +1,8 @@
 import base64
 import json
-import random
 import re
 import requests
 import time
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
@@ -93,7 +86,7 @@ class simpleSolver():
 
 
 class reCapthaSolver():
-    '''reCaptha V3 解析 -- 配合 selenium 使用'''
+    '''reCaptha V3 解析 -- 原配合 selenium 使用'''
 
     def __init__(self, SECRETID:str, SECRETKEY:str, driver, timeout=15):
         '''使用腾讯云的免费语音识别, 传入腾讯云账户的 SECRETID, SECRETKEY 和 selenium 的 driver 对象'''
@@ -104,87 +97,6 @@ class reCapthaSolver():
         msg_url = url
         result1 = upload(msg_url, self.SECRETID, self.SECRETKEY)  # 上传链接返回结果
         return result1
-
-    # 关键点，下面两函数直接套着用就行了，不用管，这里是重点
-    def solve_selen(self, XPathInfo:str):
-        '''传入验证框 iframe 对应的 XPath'''
-        # 等待加载上验证框，验证框iframe被套在一个form中
-        WebDriverWait(self.driver, self.timeout*2, 0.5).until(
-            EC.visibility_of_element_located((By.XPATH, XPathInfo)))
-        # 进入验证框所在iframe
-        time.sleep(random.uniform(0.1, 0.5))
-        self.driver.switch_to.frame(
-            self.driver.find_element(By.XPATH, XPathInfo))
-        # '//*[@id="form-submit"]/div[2]/div/div/div/iframe'))
-        print("加载验证中")
-        # 等待勾选框可点击再点击
-        WebDriverWait(self.driver, self.timeout, 0.5).until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, "#recaptcha-anchor"))).click()
-        # 随机2~3秒避免加载不出来
-        time.sleep(random.uniform(2, 3))
-        print(1)
-        try:
-            # 回到默认页面
-            self.driver.switch_to.default_content()
-            # 等待点击勾选框后的弹窗界面iframe有没有加载出来
-            WebDriverWait(self.driver, self.timeout, 0.5).until(
-                EC.visibility_of_element_located((By.XPATH, '/html/body/div[10]/div[4]/iframe')))
-            # 进入弹窗界面的iframe
-            self.driver.switch_to.frame(self.driver.find_element(
-                By.XPATH, '/html/body/div[10]/div[4]/iframe'))
-            print(2)
-            # 等待语音按钮是否加载出来，注意，这里在shadow-root里面，不可以直接用css选择器或xpath路径点击
-            WebDriverWait(self.driver, self.timeout*2, 0.5).until(
-                EC.visibility_of_element_located((By.XPATH, '//*[@id="recaptcha-audio-button"]')))
-            print(3)
-            # 选中语音按钮
-            self.driver.find_element(
-                By.XPATH, '//*[@id="recaptcha-audio-button"]')
-            # 初始化键盘事件
-            Ac = ActionChains(self.driver)
-            # tab按键选中
-            time.sleep(random.uniform(0.5, 1))
-            Ac.send_keys(Keys.TAB).perform()
-            # enter按键点击
-            time.sleep(random.uniform(0.7, 1.8))
-            Ac.send_keys(Keys.ENTER).perform()
-            print("点击了语音按钮")
-            # 等待页面跳转出现下载按钮，跳转后会出现语音下载按钮，需要捕获它的href值，它就是音频链接msg_url
-            WebDriverWait(self.driver, self.timeout*2, 0.5).until(
-                EC.visibility_of_element_located((By.XPATH, '/html/body/div/div/div[7]/a')))
-            msg_url = self.driver.find_element(
-                By.XPATH, '/html/body/div/div/div[7]/a').get_attribute("href")  # 获取链接
-            print(4)
-            result1 = upload(msg_url, self.SECRETID,
-                             self.SECRETKEY)  # 上传链接返回结果
-            time.sleep(1)
-            print(5)
-            print("识别结果为：")
-            print(result1)
-            # 等待加载填写框
-            WebDriverWait(self.driver, self.timeout, 0.5).until(
-                EC.visibility_of_element_located((By.XPATH, '//*[@id="audio-response"]')))
-            # 选中填写框
-            self.driver.find_element(
-                By.XPATH, '//*[@id="audio-response"]').send_keys(result1)
-            print(6)
-            # 随机时长，避免判断为机器人
-            time.sleep(random.uniform(1.5, 3))
-            # 等待加载verify验证按钮
-            WebDriverWait(self.driver, self.timeout, 0.5).until(
-                EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-verify-button"]')))
-            # 选中点击verify按钮
-            self.driver.find_element(
-                By.XPATH, '//*[@id="recaptcha-verify-button"]').click()
-            time.sleep(random.uniform(0.7, 2))
-            print("语音验证结束")
-        except Exception as e:
-            print(e)
-        print(7)
-        # 回到初始页面，进行下一步操作
-        self.driver.switch_to.default_content()
-        print(8)
-        return
 
 
 def upload(msg_url:str, SECRETID:str, SECRETKEY:str):
